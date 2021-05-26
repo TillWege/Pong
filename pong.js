6f8386e620f9daf5bd940506ad845fc938346588
Till <tillwege@gmail.com>
2021-05-26 20:25:24 +0200

    Works v1
    'css':
       - Ball-Startposition an die Änderung korrigiert um genau mittig zu sein
    'js':
       - Bewgung des Spieler verlangsamt um immer im "2ergrid" zu bleiben (Alternativ 4 Testen)
       - V1 der Play Reflektion implementiert die noch nicht korrek funktioniert
       - Refletktion oben und unten für sehr flache winkel möglich gemacht
       - Respawnen des Balls fertig gestellt

--- pong.js
+++ pong.js
@@ -44,18 +44,20 @@ function setupPlayers() {
 function setupGame() {
     document.onkeydown = (event) => {
         if (event.key == 'w') {
-            moveplayer(window.p1, -3)
+            moveplayer(window.p1, -2)
         } else if (event.key == 's') {
-            moveplayer(window.p1, +3)
+            moveplayer(window.p1, +2)
         } else if (event.key == 'ArrowUp') {
-            moveplayer(window.p2, -3)
+            moveplayer(window.p2, -2)
         } else if (event.key == 'ArrowDown') {
-            moveplayer(window.p2, +3)
+            moveplayer(window.p2, +2)
+        } else if (event.key == 'p') {
+            debugger
         }
     }
 
     //Gamestate Variablen (re)-initialisieren
-    vx = 20
+    vx = 7
     vy = 0
     score1 = 0
     $('#score1').text(0)
@@ -95,34 +97,54 @@ function doUpdate() {
     //Tor-Erkennung
     if (x <= 0) {
         addp2Score()
-        ball.css("left", "640px")
         x = 640;
+        ball.css("left", "640px")
+        y = getRandomInt(75, 625)
+        ball.css("top", y + "px")
+        vx = getRandomArbitrary(0.2, 0.8) * 7
+        vy = 7 - vx
+        if (RandomBoolean()) {
+            vy = -vy
+        }
     } else if (x >= 1260) {
         addp1Score()
-        ball.css("left", "640px")
         x = 640;
+        ball.css("left", "640px")
+        y = getRandomInt(75, 625)
+        ball.css("top", y + "px")
+        vx = -(getRandomArbitrary(0.2, 0.8) * 7)
+        vy = 7 + vx
+        if (RandomBoolean()) {
+            vy = -vy
+        }
     }
 
     //An dem oberen und unteren Balken refelktieren  
-    if (y < 50 || y > 650) {
+    if (y < 50) {
+        y = 50
         vy = -vy;
+    } else if (y > 650) {
+        y = 650
+        vy = -vy
     }
 
 
 
     if ((50 <= x) && (x <= 65)) {
         let p1top = parseInt(window.p1.css("top"))
-        let a = y - p1top;
-        if ((a >= -20) && (a <= 150)) {
-            vx = -vx
-            console.log("hit" + a)
+        let a = y - p1top + 20;
+        if ((a >= 0) && (a <= 170)) {
+            let rel = a - 85;
+            vx = 7 * ((100 - Math.abs(rel)) / 100)
+            vy = 7 * (rel / 100)
         }
     } else if ((1215 <= (x + 20)) && ((x + 20) <= 1230)) {
         let p2top = parseInt(window.p2.css("top"))
-        let a = y - p2top;
-        if ((a >= -20) && (a <= 150)) {
-            vx = -vx
-            console.log("hit" + a)
+        let a = y - p2top + 20;
+        if ((a >= 0) && (a <= 170)) {
+            let rel = a - 85;
+            vx = -7 * ((100 - Math.abs(rel)) / 100)
+            vy = 7 * (rel / 100)
         }
     }
 
@@ -138,8 +160,17 @@ function RandomBoolean() {
     return Math.random() < 0.5;
 }
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 function addp1Score() {
-    debugger
     score1++;
     if (score1 == 5) {
         window.alert("Spieler 1 hat gewonnen!" + "\n" + "Space zum erneut spielen")
